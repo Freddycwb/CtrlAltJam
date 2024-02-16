09015c2f9ef779626fb2502d7ea6d2118a61723c
Freddy Matheus <45660002+Freddycwb@users.noreply.github.com>
2024-02-16 05:33:44 -0300

    Adjusting Enemy

---
+++ CtrlAltJam/Assets/Scripts/Enemy/ActiveAfterPoints.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActiveAfterPoints : MonoBehaviour
+{
+    [SerializeField] private FloatVariable score;
+    [SerializeField] private float scoreToActive;
+    [SerializeField] private UnityEvent action;
+
+    private void Update()
+    {
+        if (score.value >= scoreToActive)
+        {
+            action.Invoke();
+        }
+    }
+}
